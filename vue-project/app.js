0fb7853ea19eba10c0e6a5dc813ab48a2ecca512
AngelaBurgaleta <135bangbur@gmail.com>
2022-08-13 20:09:50 +0200

    Hasta el apartado 7 del curso de Udemy

---
+++ vue-project/app.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    //data, functions
+    //template: '<h2>I am the template</h2>'
+})
+
+//apunta al componente que mostrar en el DOM
+//DOM, la estructura de objetos que genera el navegador en memoria para cada uno de los elementos de la página.
+app.mount('#app')
