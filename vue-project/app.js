a960b35430ac273cee65f862820566cd8e0bb294
AngelaBurgaleta <135bangbur@gmail.com>
2022-08-14 10:40:45 +0200

    section 2 apartado 11 no me funciona el mousemove

--- vue-project/app.js
+++ vue-project/app.js
@@ -1,8 +1,31 @@
 const app = Vue.createApp({
-    //data, functions
-    //template: '<h2>I am the template</h2>'
-})
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 56,
+      x: 0,
+      y: 0
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+  },
+  handleMousemove(e) {
+    this.x = e.offsetX;
+    this.y = e.offsetY;
+  },
+});
 
 //apunta al componente que mostrar en el DOM
 //DOM, la estructura de objetos que genera el navegador en memoria para cada uno de los elementos de la página.
-app.mount('#app')
+app.mount("#app");
