a17ebb4e0918387ec016d1ea5515d8d841abfdfd
AngelaBurgaleta <94869452+AngelaBurgaleta@users.noreply.github.com>
2022-09-03 18:43:12 +0200

    lesson12 vue course book in books

--- vue-project/app.js
+++ vue-project/app.js
@@ -2,28 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 56,
-      x: 0,
-      y: 0
+      books: [
+        { title: "The Final Empire", author: "Brandon Sanderson" },
+        {title: 'the way o kings', author: 'Patrick Rothfuss'},
+        {title: 'three tales of a chemical romance ', author: 'Irvine Wells'}]
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-  },
-  handleMousemove(e) {
-    this.x = e.offsetX;
-    this.y = e.offsetY;
-  },
+  }
 });
 
 //apunta al componente que mostrar en el DOM
