e43b22cefd19ec2f46766a84911af64e200b8b50
AngelaBurgaleta <94869452+AngelaBurgaleta@users.noreply.github.com>
2022-09-19 16:47:08 +0200

    CHALLENGE: Reusing components

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -10,11 +10,28 @@
       <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
-  </div>  
+
+  </div>
+  <div v-if="showModalTwo">
+    <Modal @closeTwo = "toggleModalTwo">
+      <h1>ModalDos</h1>
+      <template v-slot:linksTwo>
+        <a href="#">sign up now 2</a>
+        <a href="#">more info 2</a>
+      </template>
+      <button @click="toggleModalTwo">close modal two</button>
+    </Modal>
+
+  </div>
   <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
+// challenge
+//create an extra button to open a different modal
+//use the same modal component but pass in a different template (slot)
+//use a different method (toggleModaltwo) and data (showModalTwo)
 import Modal from './components/Modal'
 export default {
   name: 'App',
@@ -23,11 +40,15 @@ export default {
     return {
       title: 'My First Vue App!',
       showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
@@ -42,6 +63,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
